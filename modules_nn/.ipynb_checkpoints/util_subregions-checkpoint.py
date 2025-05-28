ba8fb7936bdd861e4467f100a72a66b2f405fb22
Reinel Sospedra-Alfonso <reinel.sospedra-alfonso@canada.ca>
2025-05-28 21:15:35 +0000

    clean slate

--- modules_nn/.ipynb_checkpoints/util_subregions-checkpoint.py
+++
@@ -1,9 +0,0 @@
-# bounding box coordinates for pre-defined subregions
-# lat_min, lat_max, lon_min, lon_max
-
-subregions = {
-    "Globe": [-90, 90, 0, 360],
-    "SPNA": [35, 65, 305, 335],
-    "TropicalPacific": [-15, 15, 135, 270],
-    'Southern' : [-90, -35, -180, 180]
-}
